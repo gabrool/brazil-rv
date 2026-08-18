a91815d08c282a2f1018c9e22a7db3731f104c55
gabrool <gabrielmetne@gmail.com>
2026-08-18 17:02:21 -0400

    Allow development-inactive target scale slots

--- research/src/brazil_rv/preprocessing/target_scale.py
+++ research/src/brazil_rv/preprocessing/target_scale.py
@@ -96,7 +96,11 @@ def build_target_scale_sidecar(
     assignments = load_assignments(assignments_dir)
     security_ids = tuple(assignments.get_column("security_id").to_list())
     market_dates, assignment_dates = load_market_dates_and_security_dates(
-        cotahist_files(cotahist_dir), security_ids, research_start, through
+        cotahist_files(cotahist_dir),
+        security_ids,
+        research_start,
+        through,
+        allow_empty_security_dates=True,
     )
     validate_source_date_isolation(assignments, assignment_dates)
     date_index = (
